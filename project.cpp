b64576a13951f52bd75662f70b22b5ebb604bd75
Asifur Rahaman <asifurrahaman754@gmail.com>
2023-10-21 23:25:43 +0600

    add login for both student and admin

--- project.cpp
+++ project.cpp
@@ -7,27 +7,82 @@ using namespace std;
 const int MAX_STUDENTS = 20; 
 
 class Student {
+private:
+    string password;
 public:
     string name;
     int id;
+    
+    void setPassword(string pass) {
+        password = pass;
+    }
+    
+    string getPassword(){
+        return password;
+    }
 };
- Student students[MAX_STUDENTS];
+
+Student students[MAX_STUDENTS];
+Student admin;
 int numStudents = 0;
+// 0 = null; 1 = admin, 2 = student;
+int loggedInUser = 0;
 
 void initialMessages();
 void processInput(int input);
 void registration();
 void viewStudents();
+void login();
+
+void login(int userRole){
+    string name;
+    string pass;
+    
+    cout << "type your name: ";
+    cin >> name;
+    cout << "type your password: ";
+    cin >> pass;
+    if(userRole == 1){
+        if(admin.name == name && admin.getPassword() == pass){
+            cout << "Welcome admin!"<< endl;
+            loggedInUser = 1;
+        }else{
+            cout << "Wrong credentials!"<<endl;
+        }
+        
+    }else if(userRole == 2){
+        int userFound = 0;
+        for(int i = 0; i < numStudents; i++){
+            if(students[i].name == name && students[i].getPassword() == pass){
+                cout << "Welcome "<< name;
+                loggedInUser = 2;
+                userFound = 1;
+                break;
+            }
+        }
+        
+        if(!userFound){
+            cout << "No data Found!"<<endl;
+        }
+    }
+    
+    initialMessages();
+}
 
 void processInput(int input){
     switch(input){
         case 1:
-            cout << "1 is selected";
+            int userRole;
+            cout << "1. Login as Admin" << endl;
+            cout << "2. Login as Student" << endl;
+            cin >> userRole;
+            login(userRole);
+            
             break;
         case 2:
             registration();
             break;
-        case 3:
+        case 4:
             viewStudents();
             break;
         default:
@@ -37,32 +92,41 @@ void processInput(int input){
 };
 
 void initialMessages(){
+    cout << "\n";
     int userInput;
-    cout << "1. Login" << endl;
-    cout << "2. Registration"<< endl;
-    cout << "2. Add book"<< endl;
-    cout << "3. View students"<< endl;
-    cout << "Please type the order number to select an option" << endl;
     
+    if(loggedInUser){
+        cout << "3. Add book"<< endl;
+        cout << "4. View students"<< endl;
+    }else{
+        cout << "1. Login" << endl;
+        cout << "2. Registration"<< endl;
+    }
+    cout << "Please type the order number to select an option" << endl;
     cin >> userInput;
     
-    processInput(userInput);
+    if(!loggedInUser && userInput > 2){
+        cout << "please login!" << endl;
+        initialMessages();
+    }else{
+        processInput(userInput);
+    }
 }
 
 void registration(){
-    string name;
-    int id;
+    string pass;
     
     cout << "type your name: ";
-    cin >> name;
-    
-        // Clear the newline character from the input buffer
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
-    
-    students[numStudents].name = name;
+    cin >> students[numStudents].name;
     cout << "type your Id: ";
-    cin >> id;
-    students[numStudents].id = id;
+    cin >> students[numStudents].id;
+    cout << "type your password: ";
+    cin >> pass;
+    
+    // Clear the newline character from the input buffer
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    students[numStudents].setPassword(pass);
+    
     numStudents++;
     cout << "student added :)"<<endl;
     initialMessages();
@@ -70,18 +134,20 @@ void registration(){
 
 void viewStudents(){
     if(numStudents == 0){
-        cout << "no student found";
+        cout << "no student found" << endl;
     }else{
         for(int i = 0; i< numStudents; i++){
             cout << "name is:"<< students[i].name << endl;
             cout << "id is:"<< students[i].id << endl;
         }
     }
+    initialMessages();
 }
 
-
-
 int main() {
+    // admin by default
+    admin.name = "admin";
+    admin.setPassword("1234");
     initialMessages();
 
     return 0;
